648d54ba791722b02de520c6728dc4e6e04a7389
Biswajeet Mandal <biswajeet163@gmail.com>
2021-02-06 22:54:02 +0530

    First Commit

---
+++ src/test/java/com/jwtauth/model/UserCredentialTest.java
@@ -0,0 +1,32 @@
+package com.jwtauth.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserCredentialTest 
+{
+	UserCredential userHospital=new UserCredential();
+	
+	@Test
+	public void testUserHospitalAllConstructor()
+	{
+		UserCredential hospital=new UserCredential("ab", "ab", "role");
+		assertEquals("ab",hospital.getUsername());
+	}
+	
+	
+	
+	
+	 
+	@Test
+	public void testoString() {
+		String string = userHospital.toString();
+		assertEquals(userHospital.toString(),string);
+	}
+}
